dd47296eaa90f6936d74c03ef3866dffe2f8bc7f
slakshya-22 <slakshyamainacc@gmail.com>
2025-04-14 22:47:36 +0530

    Changed port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { nanoid } = require('nanoid');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const urlDatabase = {};
 
